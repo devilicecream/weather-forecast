46d976e355ebe086b0e84b920b970d20d66b076c
dimolf345 <dimolfetta.ta@gmail.com>
2020-06-18 19:49:49 +0200

    Update forecast.js

--- js/forecast.js
+++ js/forecast.js
@@ -1,6 +1,13 @@
 const citySearch = document.querySelector("#citySearch");
 const searchBtn = document.querySelector("#searchBtn");
 
+
+const KELVIN = 273;
+const key = '&appid=436faa03983b144a87745bd11c39811e';
+
+let baseURL = 'http://api.openweathermap.org/data/2.5/forecast?q=';
+let weatherInfo= {}
+
 citySearch.addEventListener('keyup', function (e) {
   e.preventDefault();
   if (e.keyCode === 13) {
@@ -9,6 +16,24 @@ citySearch.addEventListener('keyup', function (e) {
   })
 
 searchBtn.addEventListener('click', function() {
-  alert(citySearch.value);
-  citySearch.value = "";
+  let db= downloadWeather(citySearch.value);
+  console.log(weatherInfo);
 });
+
+
+
+function downloadWeather(cityName) {
+  let url = baseURL + cityName.toLowerCase() + key;
+  fetch(url)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function (data) {
+    weatherInfo = data.list;
+    return weatherInfo;
+    })
+  .catch(function (error) {
+    alert('Something went wrong');
+  })
+  return weatherInfo;
+}
